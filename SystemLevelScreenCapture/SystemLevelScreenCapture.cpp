25b90b3e1b5880b57128a6446dacbebba55334b4
GoldenFlash0824 <codeexpert0824@gmail.com>
2024-06-13 14:13:01 -0700

    H264 compression has been added

--- SystemLevelScreenCapture/SystemLevelScreenCapture.cpp
+++ SystemLevelScreenCapture/SystemLevelScreenCapture.cpp
@@ -1,3 +1,9 @@
+extern "C" {
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libswscale/swscale.h>
+}
+
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include <windows.h>
@@ -9,11 +15,11 @@
 #pragma comment(lib, "Ws2_32.lib")
 #pragma comment(lib, "Gdi32.lib")
 
-#define SERVER_PORT 12345
+#define SERVER_PORT 8762
 #define SERVER_IP "127.0.0.1"
 
-// Function to send bitmap data to the server
-bool SendBitmapData(SOCKET clientSocket, const BYTE* data, int dataSize) {
+// Function to send encoded data to the server
+bool SendEncodedData(SOCKET clientSocket, const uint8_t* data, int dataSize) {
     int bytesSent = 0;
     while (bytesSent < dataSize) {
         int result = send(clientSocket, reinterpret_cast<const char*>(data + bytesSent), dataSize - bytesSent, 0);
@@ -26,7 +32,7 @@ bool SendBitmapData(SOCKET clientSocket, const BYTE* data, int dataSize) {
     return true;
 }
 
-void CaptureScreen(SOCKET clientSocket) {
+void CaptureAndEncodeScreen(SOCKET clientSocket, AVCodecContext* codecContext, AVFrame* frame, AVPacket* pkt) {
     HDC hScreenDC = GetDC(NULL);
     if (!hScreenDC) {
         std::cerr << "GetDC failed: " << GetLastError() << std::endl;
@@ -65,7 +71,7 @@ void CaptureScreen(SOCKET clientSocket) {
     bi.biWidth = screenWidth;
     bi.biHeight = -screenHeight;
     bi.biPlanes = 1;
-    bi.biBitCount = 32;
+    bi.biBitCount = 24;
     bi.biCompression = BI_RGB;
     int dataSize = ((screenWidth * bi.biBitCount + 31) / 32) * 4 * screenHeight;
     BYTE* pData = new BYTE[dataSize];
@@ -79,16 +85,27 @@ void CaptureScreen(SOCKET clientSocket) {
         return;
     }
 
-    BITMAPFILEHEADER bfh;
-    memset(&bfh, 0, sizeof(bfh));
-    bfh.bfType = 0x4D42; // 'BM'
-    bfh.bfOffBits = sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER);
-    bfh.bfSize = bfh.bfOffBits + dataSize;
+    SwsContext* swsContext = sws_getContext(screenWidth, screenHeight, AV_PIX_FMT_BGR24,
+        screenWidth, screenHeight, AV_PIX_FMT_YUV420P,
+        SWS_BILINEAR, NULL, NULL, NULL);
 
-    send(clientSocket, reinterpret_cast<const char*>(&bfh), sizeof(bfh), 0);
-    send(clientSocket, reinterpret_cast<const char*>(&bi), sizeof(bi), 0);
-    SendBitmapData(clientSocket, pData, dataSize);
+    uint8_t* srcData[1] = { pData };
+    int srcLinesize[1] = { 3 * screenWidth };
 
+    sws_scale(swsContext, srcData, srcLinesize, 0, screenHeight, frame->data, frame->linesize);
+
+    frame->pts++;
+
+    if (avcodec_send_frame(codecContext, frame) < 0) {
+        std::cerr << "Error sending frame to encoder" << std::endl;
+    }
+
+    while (avcodec_receive_packet(codecContext, pkt) == 0) {
+        SendEncodedData(clientSocket, pkt->data, pkt->size);
+        av_packet_unref(pkt);
+    }
+
+    sws_freeContext(swsContext);
     delete[] pData;
     SelectObject(hMemoryDC, hOldBitmap);
     DeleteObject(hBitmap);
@@ -96,7 +113,6 @@ void CaptureScreen(SOCKET clientSocket) {
     ReleaseDC(NULL, hScreenDC);
 }
 
-// Function to capture the entire screen and send data to Electron
 int main() {
     WSADATA wsaData;
     SOCKET clientSocket = INVALID_SOCKET;
@@ -130,11 +146,77 @@ int main() {
         return 1;
     }
 
+    const AVCodec* codec = avcodec_find_encoder(AV_CODEC_ID_H264);
+    if (!codec) {
+        std::cerr << "Codec not found" << std::endl;
+        closesocket(clientSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    AVCodecContext* codecContext = avcodec_alloc_context3(codec);
+    if (!codecContext) {
+        std::cerr << "Could not allocate video codec context" << std::endl;
+        closesocket(clientSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    codecContext->bit_rate = 400000;
+    codecContext->width = GetSystemMetrics(SM_CXSCREEN);
+    codecContext->height = GetSystemMetrics(SM_CYSCREEN);
+    codecContext->time_base = { 1, 30 };
+    codecContext->framerate = { 30, 1 };
+    codecContext->gop_size = 10;
+    codecContext->max_b_frames = 1;
+    codecContext->pix_fmt = AV_PIX_FMT_YUV420P;
+
+    if (avcodec_open2(codecContext, codec, NULL) < 0) {
+        std::cerr << "Could not open codec" << std::endl;
+        avcodec_free_context(&codecContext);
+        closesocket(clientSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    AVFrame* frame = av_frame_alloc();
+    if (!frame) {
+        std::cerr << "Could not allocate video frame" << std::endl;
+        avcodec_free_context(&codecContext);
+        closesocket(clientSocket);
+        WSACleanup();
+        return 1;
+    }
+    frame->format = codecContext->pix_fmt;
+    frame->width = codecContext->width;
+    frame->height = codecContext->height;
+    if (av_frame_get_buffer(frame, 32) < 0) {
+        std::cerr << "Could not allocate the video frame data" << std::endl;
+        av_frame_free(&frame);
+        avcodec_free_context(&codecContext);
+        closesocket(clientSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    AVPacket* pkt = av_packet_alloc();
+    if (!pkt) {
+        std::cerr << "Could not allocate AVPacket" << std::endl;
+        av_frame_free(&frame);
+        avcodec_free_context(&codecContext);
+        closesocket(clientSocket);
+        WSACleanup();
+        return 1;
+    }
+
     while (true) {
-        CaptureScreen(clientSocket);
+        CaptureAndEncodeScreen(clientSocket, codecContext, frame, pkt);
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
 
+    av_packet_free(&pkt);
+    av_frame_free(&frame);
+    avcodec_free_context(&codecContext);
     closesocket(clientSocket);
     WSACleanup();
     return 0;
